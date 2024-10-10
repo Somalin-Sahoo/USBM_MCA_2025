da4862a31e965c80e4e2635c1f697d2ed852ae5f
Somalin sahoo <somalinsahoo2002@gmail.com>
2024-10-10 19:59:52 +0530

    commit

---
+++ frontend/Javascript/class-1/index.js
@@ -0,0 +1,65 @@
+var name ="payal";
+
+// let age=22;
+const id=1;
+const city = "Bhubaneswar";
+
+
+// console.log(address);
+
+var address ="Bhubaneswar";
+//console.log(address);
+
+//Null
+//let user;
+
+let user =null;
+//var user =null;
+//const user =null;
+//console.log(typeof null); //object
+//console.log(user);
+ 
+
+
+
+//undefined
+let data;
+//console.log(typeof data ); //undefined
+//console.log(data);
+
+//Number
+let num1 =50;
+let decimal_no = 1.0001;
+let pi = 3.14;
+let age = 25;
+let roll_no = 21;
+//console.log(typeof num1); //number
+
+//Addition
+//let num2 = 100;
+//let num3 = 125;
+//let additiom = num2+num3;
+//let substraction =num2 - num3;
+//let mul = num2 * num3;
+//let div = num2 / num3;
+//console.log(addition); 
+//console.log(substraction);
+//console.log(mul);
+//console.log(div);
+
+//Bignit
+let huge_no=34567666666666;
+console.log(typeof huge_no);
+
+//Symbol
+let sym1 = Symbol("sym1");
+let sym2 = Symbol("sym2");
+console.log(sym1 === sym2); //false
+
+//NaN
+let num5 = 0;
+let num6 = 0;
+console.log(num5 / num6);
+
+
+
